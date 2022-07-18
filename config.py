f40a8406e137d2244d562d890d342f1e685f3a3b
FaresAT <59063618+FaresAT@users.noreply.github.com>
2022-07-18 03:43:06 -0400

    update config.py

--- config.py
+++ config.py
@@ -1,24 +1,14 @@
-# import os
-#
-#
-# basedir = os.path.abspath(os.path.dirname(__file__))
-#
-#
-# class Config(object):
-#     SECRET_KEY = os.environ.get("SECRET_KEY") or "NO_KEY"
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
-#                               or 'sqlite:///' + os.path.join(basedir, 'app.db')
-#                               # or "jdbc: sqlite:/home/fares/PycharmProjects/blog/app.db"
-#
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
 import os
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "NO_KEY"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
+                              or 'sqlite:///' + os.path.join(basedir, 'app.db')
+                              # or "jdbc: sqlite:/home/fares/PycharmProjects/blog/app.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
